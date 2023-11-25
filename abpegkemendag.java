951a1499134e157c696e9856c96956a7854922ce
ferryamludn2 <ferryamludn@gmail.com>
2023-11-25 14:14:11 +0700

    add projek

---
+++ abpegkemendag.java
@@ -0,0 +1,22 @@
+import java.util.Vector;
+
+public class abpegkemendag {
+	private int _iD;
+	private list _listPegawai;
+	private data _dataAbsensi;
+	public server _svr;
+	public jaringan_kemendag _jrg;
+	public Vector<pegawai> _pgw = new Vector<pegawai>();
+
+	public void recordKehadiran() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void melihatDataAbsensi() {
+		throw new UnsupportedOperationException();
+	}
+
+	public void mendaftarkanSidikJari() {
+		throw new UnsupportedOperationException();
+	}
+}
